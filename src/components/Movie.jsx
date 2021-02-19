ff7986fdc9f4fd45d32f8f2d32f6c2aa9d0c35ca
Bryan Molk <brybrym92@MacBook-Pro-3.fios-router.home>
2021-02-19 15:23:06 -0500

    updated home page with css

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import axios from "axios";
+import "./Movie.css";
 import { baseURL, config } from "../services";
 
 function Movie(props) {
@@ -16,8 +17,8 @@ function Movie(props) {
     <div className="movie-list" key={props.movie.id}>
       <img
         className="img-class" src={image ? image : "https://i.imgur.com/fs3wLRO.jpg"} alt="" />
-      <h3>Title: {title }</h3>
-      <h5>Streaming-on: {streaming_on}</h5>
+      <h4>{title }</h4>
+      <h5>{streaming_on}</h5>
       <Link to={`/movies/${props.movie.id}`}>
         <button >MovieInfo</button>
       </Link>
