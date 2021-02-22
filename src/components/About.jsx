6f61aebe4edbce24a98f3fb47cdb897b276e770d
Bryan Molk <brybrym92@MacBook-Pro-3.fios-router.home>
2021-02-21 19:41:20 -0500

    created a media query for tablet/Ipad size

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,16 @@
 function About() {
   return (
     <div className="about-container">
-       <div className="about-class">
-      <h2>Welcome To stream-it</h2>
-      <p>stream-it is an Airtable and
-      react app to suggest favorite
-      movies and shows on different
-      streaming services so users can
-      watch it and also where a user
-      can be able to post thier favorite
-      movies and where in the stream
-      it is avaialable and also rate
-      the movie or the show how based on
-        how they feel about it.</p>
+      <div className="about-class">
+        <h2>Welcome To stream-it</h2>
+        <p>
+          stream-it is an Airtable and react app to suggest favorite movies and
+          shows on different streaming services so users can watch it and also
+          where users could be able to post their favorite movies and where in
+          the streaming service the movies are avaialable.
+        </p>
+      </div>
     </div>
-    </div>
-   
-  )
+  );
 }
 export default About;
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -15,14 +15,16 @@ function Movie(props) {
   return (
     <div className="movie-list" key={props.movie.id}>
       <img
-        className="img-class" src={image ? image : "https://i.imgur.com/fs3wLRO.jpg"} alt="" />
+        className="img-class"
+        src={image ? image : "https://i.imgur.com/fs3wLRO.jpg"}
+        alt=""
+      />
       <h4 className="h4-class">{title}</h4>
       <h5 className="h5-class">{streaming_on}</h5>
       <Link to={`/movies/${props.movie.id}`}>
-        <button >MovieInfo</button>
+        <button>MovieInfo</button>
       </Link>
       {/* <button onClick={deleteMovie}>Delete!</button> */}
-
     </div>
   );
 }
